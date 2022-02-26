d2826a5027149608c2582a20dc1e1214a73edea0
Mike <mike@Zipcoders-MacBook-Pro.local>
2022-02-26 10:35:44 -0500

    More basic calc tests

--- src/main/java/BasicCalc.java
+++ src/main/java/BasicCalc.java
@@ -4,6 +4,7 @@ public class BasicCalc {
     }
 
     static double subTract(double num1, double num2) {
+        System.out.println(num1 - num2);
         return num1 - num2;
     }
 
--- src/test/java/BasicCalcTest.java
+++ src/test/java/BasicCalcTest.java
@@ -12,4 +12,30 @@ class BasicCalcTest {
         assertEquals(10,BasicCalc.subTract(20,10));
 
     }
+    /*@org.junit.jupiter.api.Test
+    public void multiply() {BasicCalc multiply = new BasicCalc();
+        assertEquals(200,BasicCalc.multiply(20,10));
+
+    }
+    @org.junit.jupiter.api.Test
+    public void divide() {BasicCalc divide = new BasicCalc();
+        assertEquals(2,BasicCalc.divide(20,10));
+
+    }
+    @org.junit.jupiter.api.Test
+    public void square() {BasicCalc square = new BasicCalc();
+        assertEquals(4,BasicCalc.square(2));
+
+    }
+    @org.junit.jupiter.api.Test
+    public void squareRoot() {BasicCalc squareRoot = new BasicCalc();
+        assertEquals(2,BasicCalc.squareRoot(4));
+
+    }
+    @org.junit.jupiter.api.Test
+    public void inverse() {BasicCalc inverse = new BasicCalc();
+        assertEquals(-10,BasicCalc.inverse(10));
+
+    }*/
+
 }
