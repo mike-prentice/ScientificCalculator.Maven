ae885cc048fa59672786ec1efdc18275585e773e
Miles Coney <MilesEConey@gmail.com>
2022-02-26 19:25:58 -0500

    Updated scientific calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 
     double output;
 
-    public double Sine(double input) {
+    public double sine(double input) {
         output = Math.sin(Math.toRadians(input));
         return output;
     }
@@ -14,6 +14,42 @@ public class Calculator {
 
     public double tangent(double input) {
         output = Math.tan(Math.toRadians(input));
+        return output;
+    }
+
+
+    public double inverseSine(double input) {
+        output = (Math.asin(input) * (180/Math.PI));
+        return output;
+    }
+    public double inverseCosine(double input) {
+        output = (Math.acos(input) * (180/Math.PI));
+        return output;
+    }
+
+    public double naturalLog(double input) {
+        output = Math.log(input);
+        return output;
+    }
+
+    public double log(double input) {
+        output = Math.log10(input);
+        return output;
+
+    }
+
+    public double inverseLn(double input) {
+        output = Math.exp(input);
+        return output;
+    }
+
+    public double inverseLog(double input) {
+        output = Math.pow(10, input);
+        return output;
+    }
+
+    public double cubeRoot(double input) {
+        output = Math.cbrt(input);
         return output;
     }
 }
